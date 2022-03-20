17eb4899f5e73d1b35314374002cec8634af694d
Maroš Janota <maros.janota@chyronhego.com>
2022-03-20 18:01:38 +0100

    Menu update

--- src/views/Daily.vue
+++ src/views/Daily.vue
@@ -1,36 +1,37 @@
 <template>
-    <Section>
-         <Box title="Polední menu">
-            <div class="menu" v-for="m in getMenu" :key="m.id" > 
-                <MenuItem :menu="m" :repeated="getRepeated"/>
-            </div>
-            <div class="d-f jc-c mt-xl">
-                <p class="text--bold mt-lôg">Polévka k menu 15kč</p>    
-            </div>
-        </Box>
-    </Section>
+  <Section>
+    <Box title="Polední menu">
+      <div class="menu" v-for="m in getMenu" :key="m.id">
+        <MenuItem :menu="m" :repeated="getRepeated" />
+      </div>
+      <div class="d-f jc-c mt-xl">
+        <p class="text--bold mt-lôg">Polévka k menu malá 15Kč, velká 20kč</p>
+      </div>
+      <!-- <h3>28.2. - 4.3.2022 &nbsp;&nbsp; Zavřeno</h3> -->
+    </Box>
+  </Section>
 </template>
 <script>
-import { Section, MenuItem, Box } from '@/components' 
+import { Section, MenuItem, Box } from "@/components";
 
 export default {
-    components: {
-        Section,
-        MenuItem,
-        Box,
+  components: {
+    Section,
+    MenuItem,
+    Box,
+  },
+  computed: {
+    getMenu() {
+      return this.$store.getters.getMenu;
     },
-    computed: {
-        getMenu() {
-            return this.$store.getters.getMenu
-        },
-        getRepeated() {
-            return this.$store.getters.getRepeated
-        }
-    }
-}
+    getRepeated() {
+      return this.$store.getters.getRepeated;
+    },
+  },
+};
 </script>
 <style lang="scss">
-    .menu + .menu {
-        margin-top: var(--gap-lg);
-    }
+.menu + .menu {
+  margin-top: var(--gap-lg);
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,41 @@
 <template>
   <Section>
-    <Box title="Dnešní menu" v-if="getCurrentDay" >
-      <MenuItem :menu="getCurrentDay" :repeated="getRepeated" v-if="getCurrentDay"/>
-        <div class="d-f jc-c mt-xl">
-            <p class="text--bold mt-lôg">Polévka k menu 15kč</p>    
-        </div>
+    <Box title="Dnešní menu" v-if="getCurrentDay">
+      <MenuItem
+        :menu="getCurrentDay"
+        :repeated="getRepeated"
+        v-if="getCurrentDay"
+      />
+      <div class="d-f jc-c mt-xl" date="20.03.2022">
+        <p class="text--bold mt-lôg">Polévka k menu malá 15Kč, velká 20kč</p>
+      </div>
+      <!-- <h3>28.2. - 4.3.2022 &nbsp;&nbsp; Zavřeno</h3> -->
     </Box>
-    <Gallery/>
-    <Contact/>
+    <Gallery />
+    <Contact />
   </Section>
-
 </template>
 
 <script>
-import { Section, Box, MenuItem } from "@/components"
-import Contact from "@/views/Contact.vue"
-import Gallery from "@/views/Gallery.vue"
+import { Section, Box, MenuItem } from "@/components";
+import Contact from "@/views/Contact.vue";
+import Gallery from "@/views/Gallery.vue";
 
 export default {
   components: {
-      Section,
-      Box,
-      Contact,
-      MenuItem,
-      Gallery
+    Section,
+    Box,
+    Contact,
+    MenuItem,
+    Gallery,
   },
   computed: {
-      getCurrentDay () {
-          return this.$store.getters.getCurrentDay
-      },
-      getRepeated () {
-        return this.$store.getters.getRepeated
-      }
-  }
-}
+    getCurrentDay() {
+      return this.$store.getters.getCurrentDay;
+    },
+    getRepeated() {
+      return this.$store.getters.getRepeated;
+    },
+  },
+};
 </script>
