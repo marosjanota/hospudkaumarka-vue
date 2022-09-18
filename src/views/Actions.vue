a46398f70312f8ff202286413efb1c6b29eec3e1
Maroš Janota <maros.janota@chyronhego.com>
2022-09-18 12:17:12 +0200

    edited kukoptik promo

--- src/views/Actions.vue
+++ src/views/Actions.vue
@@ -1,5 +1,17 @@
 <template>
   <Box title="" class="gallery__action" v-if="visible">
+    <h2>
+      <span class="text--bold" style="color: #9b47b2">KUK OPTIK</span> - Nová
+      oční optika v Chrudimi
+    </h2>
+    <p>
+      Kompletní péče o Váš zrak na jednom místě - meření zraku, prodej brýlí a
+      kontaktních čoček.
+    </p>
+    <p>
+      Více informac na:
+      <a href="https://kukoptik.cz/" target="_blank">kukoptik.cz</a>
+    </p>
     <VuePictureSwipe :items="items"> </VuePictureSwipe>
   </Box>
 </template>
@@ -26,10 +38,10 @@ export default {
 
     const items = [
       {
-        src: "akce/kukoptik_chrudim.png",
-        thumbnail: "akce/kukoptik_chrudim.png",
-        w: 900,
-        h: 1273,
+        src: "akce/banner_sarka_a_terka.png",
+        thumbnail: "akce/banner_sarka_a_terka.png",
+        w: 1200,
+        h: 628,
       },
     ];
 
